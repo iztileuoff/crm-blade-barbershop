31d4276635b4326cd8420b10f169057d9cf79774
RasulbekGit <rasuliztleuov@gmail.com>
2026-05-05 19:21:29 +0500

    feat: create UserSeeder to initialize super admin and admin accounts

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,19 +12,19 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::updateOrCreate(
-            ['phone' => '998999999999'],
+            ['phone' => '998906591828'],
             [
                 'name' => 'Супер Админ',
-                'password' => Hash::make('11223344'),
+                'password' => Hash::make('840798792'),
                 'role' => Role::SUPER_ADMIN,
             ]
         );
 
         User::updateOrCreate(
-            ['phone' => '998888888888'],
+            ['phone' => '998991146881'],
             [
                 'name' => 'Админ',
-                'password' => Hash::make('11223344'),
+                'password' => Hash::make('4688112688'),
                 'role' => Role::ADMIN,
             ]
         );
